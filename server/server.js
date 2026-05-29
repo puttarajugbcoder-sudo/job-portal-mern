765becc42885aa4218377ee4f48a71d81b4a573a
Puttaraju <puttarajugbcoder@gmail.com>
2026-05-29 19:51:53 +0530

    Setup React frontend and Express backend with MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,29 @@
+//import nodejs framework
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db")
+
+dotenv.config();
+connectDB();
+
+//server object
+const app = express();
+
+//middleware to parse incoming request body in JSON format.Express converts incoming JSON into a JavaScript object. without this middleware, req.body would be undefined for JSON requests.
+app.use(express.json());
+
+//Used to fetch data app.get() method is used
+//Used to send/save data app.post() method is used
+app.get("/", (req, res) => {
+    res.send("Job Portal API Running...");
+});
+app.post("/test", (req, res) => {
+    console.log(req.body);
+    res.send("Data recieved successfully");
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
