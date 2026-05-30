b095831508857f0f372c71fdcd53d77ae3a881a8
Puttaraju <puttarajugbcoder@gmail.com>
2026-05-30 20:28:54 +0530

    authcontroller

---
+++ server/controllers/authController.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
+
+        const userExists = await user.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const user = await user.create({
+            name,
+            email,
+            password,
+            role
+        })
+        res.status(201).json({ message: "User registered successfully", user });
+    }
+    catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+module.exports = { registerUser };
